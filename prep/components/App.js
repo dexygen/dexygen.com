de020562ba8f7291a61ac0390dfa84ba7fa649fa
George M. Jempty <consult@dexygen.com>
2019-12-23 23:06:43 +0000

    Numerous structural and look and feel changes including new footer component

--- prep/components/App.js
+++ prep/components/App.js
@@ -1,16 +1,22 @@
 import DexHeader from '../components/structure/dex-header.js';
+import DexFooter from '../components/structure/dex-footer.js';
 import Home from '../components/content/home.js';
 
 export default {
   name: 'App',
-  components: { DexHeader, Home },
+  components: { DexHeader, DexFooter, Home },
   template: `
-    <div class="main-container w-11/12 sm:w-3/4 lg:w-3/4 xl:w-2/3 m-auto flex-1">
-      <div class="left-gutter inline-block"></div>
-      <div class="inline-block min-w-full">
-        <main class="flex flex-col">
-          <DexHeader />
-          <Home />
+    <div class="h-screen flex flex-col">
+      <div class="main-container w-11/12 sm:w-3/4 lg:w-3/4 xl:w-2/3 m-auto flex-1">
+        <div class="left-gutter inline-block"></div>
+        <main class="inline-block min-w-full">
+          <div class="flex flex-col">
+            <DexHeader />
+            <div class="app--content">
+              <Home />
+            </div>
+            <DexFooter />
+          </div>
         </main>
       </div>
     </div>
--- prep/components/structure/dex-header.js
+++ prep/components/structure/dex-header.js
@@ -1,6 +1,6 @@
 const template = `
-  <div>
-    <header class="heading-green flex flex-col lg:flex-row lg:justify-between px-2">
+  <header>
+    <div class="header--top flex flex-col lg:flex-row lg:justify-between px-2">
       <div class="text-5xl md:text-6xl">
         <span class="tomorrow-sans-font font-bold">Dexygen</span>
       </div>
@@ -9,7 +9,7 @@ const template = `
         <a class="mr-1" href="./index.html">About</a>
         <a href="./index.html">Contact</a>
       </nav>
-    </header>
+    </div>
 
     <div class="ml-logo-row logo-tagline-container">
       <img class="inline-block" src="./assets/images/dexygen-min.png" alt=""/>
@@ -20,7 +20,7 @@ const template = `
         </div>
       </div>
     </div>
-  </div>
+  </header>
 `;
 
 export default {
